9e4edb2c78a5a09bfcfb1cb5a018beacc9365542
SAMARIN Artem <Artyom.Samarin@vaz.ru>
2021-12-01 14:59:32 +0400

    Базовые настройки приложения.

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react"
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from "react-native"
 
 const App: React.FC = () => (
   <View style={styles.container}>
-    <Text style={{ color: "#fff" }}>Hello, world!</Text>
-    <StatusBar style="auto" />
+    <Text style={{ color: "#fff" }}>Привет, мир!</Text>
+    <StatusBar style="light" />
   </View>
-);
+)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#111",
+    backgroundColor: "#33334C",
     alignItems: "center",
     justifyContent: "center",
   },
-});
+})
 
-export default App;
+export default App
